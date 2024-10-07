0aa77f4894016d3531ecc4ced9f28c4fbe99f8f1
geekyharsh05 <hp424805@gmail.com>
2024-10-07 23:25:22 +0530

    feat: Add environment variables for database configuration.

--- config/database.go
+++ config/database.go
@@ -2,25 +2,56 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	"crudapi/helper"
+
+	"github.com/joho/godotenv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbName   = "postgres"
-)
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func DatabaseConnection() *gorm.DB {
-	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
-	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{})
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		host := os.Getenv("DB_HOST")
+		if host == "" {
+			host = "postgres" // change to localhost if you are using docker local postgres database
+		}
+
+		port := os.Getenv("DB_PORT")
+		if port == "" {
+			port = "5432"
+		}
+
+		user := os.Getenv("DB_USER")
+		if user == "" {
+			user = "postgres"
+		}
+
+		password := os.Getenv("DB_PASSWORD")
+		if password == "" {
+			password = "postgres"
+		}
+
+		dbName := os.Getenv("DB_NAME")
+		if dbName == "" {
+			dbName = "postgres"
+		}
+
+		databaseURL = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+	}
+
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 	helper.ErrorPanic(err)
 
 	return db
 }
- 
--- response/tags_response.go
+++ response/tags_response.go
@@ -4,3 +4,4 @@ type TagsResponse struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
+
