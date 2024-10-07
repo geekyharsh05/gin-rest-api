b1b0bc311cec767430f7ea108b0326d17bd19eac
geekyharsh05 <hp424805@gmail.com>
2024-10-07 22:24:07 +0530

    feat: Add request and response structs for tags

---
+++ service/tags_service.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"crudapi/data/request"
+	"crudapi/data/response"
+)
+
+type TagsService interface {
+	Create(tags request.CreateTagsRequest)
+	Update(tags request.UpdateTagsRequest)
+	Delete(tagsId int)
+	FindById(tagsId int) response.TagsResponse
+	FindAll() []response.TagsResponse
+}
